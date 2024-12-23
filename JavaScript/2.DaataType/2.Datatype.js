27b197f06c020d3550fbc95dccab644a5ca548ee
Parchauhan00 <vipchauhan00@gmail.com>
2024-12-23 07:28:07 -0800

    Adding

---
+++ JavaScript/2.DaataType/2.Datatype.js
@@ -0,0 +1,27 @@
+let a = 25;  // 
+a = 20     // number
+a = "Name"  // string
+
+
+
+ a = false  // boolean type
+
+console.log("The value of a is ",a);
+console.log("The type of a is ", typeof(a));
+
+// type conversion
+
+b = 2
+console.log("Type of b",typeof(b));
+
+b = String(b);
+console.log(b);
+console.log("Type of b",typeof(b));
+b = Boolean(b)
+console.log("Type of b",typeof(b));
+
+// undefined
+let b;
+
+// null
+let b1 = null;
